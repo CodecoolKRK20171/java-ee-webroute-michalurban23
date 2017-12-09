bd58c395b188f37b2922ac2feec1bd6014cb8a7f
Michał Urban <michalurban23@gmail.com>
2017-12-09 19:25:58 +0100

    Add methods to handle forms

--- src/main/java/com/michalurban/RequestHandler.java
+++ src/main/java/com/michalurban/RequestHandler.java
@@ -1,19 +1,37 @@
 package com.michalurban;
 
 import com.sun.net.httpserver.HttpExchange;
-import java.io.IOException;
-import java.io.OutputStream;
+
+import java.io.*;
+import java.net.URLDecoder;
+
 import com.michalurban.WebRoute.METHOD;
 
 class RequestHandler {
 
-    HttpExchange httpExchange;
-    String response;
+    private HttpExchange httpExchange;
+    private HtmlPage display = new HtmlPage();
+    private String response = null;
 
-    @WebRoute(path="/a", method=METHOD.GET)
-    void a() {
+    @WebRoute(path="/form", method=METHOD.POST)
+    void showResult() throws IOException {
 
-        response = "dupa";
+        String message = getMessageFromForm();
+        String reversed = reverseMessage(message);
+
+        response = display.getResult(reversed);
+    }
+
+    @WebRoute(path="/form")
+    void showForm() {
+
+        response = display.getForm();
+    }
+
+    @WebRoute()
+    void showMainPage() {
+
+        response = display.getMainPage();
     }
 
     void setupExchange(HttpExchange httpExchange) {
@@ -21,12 +39,45 @@ class RequestHandler {
         this.httpExchange = httpExchange;
     }
 
+    void redirect() throws IOException {
+
+        httpExchange.getResponseHeaders().set("Location", "/");
+        httpExchange.sendResponseHeaders(302, -1);
+    }
+
     void sendResponse() throws IOException {
 
-        httpExchange.sendResponseHeaders(200, response.length());
+        if (response == null) {
+            redirect();
+        }
+
+        String fullResponse = display.getFullResponse(response);
+
+        httpExchange.sendResponseHeaders(200, fullResponse.getBytes().length);
         OutputStream os = httpExchange.getResponseBody();
-        os.write(response.getBytes());
+        os.write(fullResponse.getBytes());
         os.close();
+    }
+
+    private String getMessageFromForm() throws IOException {
+
+        InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
+        BufferedReader br = new BufferedReader(isr);
+        String formData = br.readLine();
+
+        String message = formData.split("=")[1];
+
+        return URLDecoder.decode(message, "UTF-8");
+    }
+
+    private String reverseMessage(String message) {
+
+        StringBuilder reversedString = new StringBuilder();
+
+        for (char c : message.toCharArray()) {
+            reversedString.insert(0, c);
+        }
+        return reversedString.toString();
     }
 
 }
