6ac107439d35cde6ee0197de41a594472ea22db2
Michał Urban <michalurban23@gmail.com>
2017-12-09 18:51:14 +0100

    Add check of request method

--- src/main/java/com/michalurban/RequestRouter.java
+++ src/main/java/com/michalurban/RequestRouter.java
@@ -2,20 +2,75 @@ package com.michalurban;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import com.michalurban.WebRoute.METHOD;
 
 public class RequestRouter implements HttpHandler {
 
-    RequestHandler handler = new RequestHandler();
+    private RequestHandler handler = new RequestHandler();
+    private String requestPath;
+    private String requestMethod;
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
 
+        boolean routeFound = false;
+        this.requestPath = httpExchange.getRequestURI().toString();
+        this.requestMethod = httpExchange.getRequestMethod();
+
         handler.setupExchange(httpExchange);
 
-        handler.a();
-        handler.sendResponse();
+        try {
+            routeFound = routeRequest();
+        } catch (ReflectiveOperationException e) {
+            e.printStackTrace();
+        }
+
+        if (routeFound) {
+            handler.sendResponse();
+        } else {
+            handler.redirect();
+        }
+    }
+
+    private boolean routeRequest() throws ReflectiveOperationException{
+
+        Class requestHandler = RequestHandler.class;
+        Method[] methods = requestHandler.getDeclaredMethods();
+        List<Method> routes = getAnnotatedMethods(methods);
+
+        for (Method route : routes) {
+            if (checkIfRequestedMethod(route)) {
+                route.invoke(handler);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private List<Method> getAnnotatedMethods(Method[] methods) {
+
+        ArrayList<Method> routes = new ArrayList<>();
+
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(WebRoute.class))
+                routes.add(method);
+        }
+        return routes;
+    }
+
+    private boolean checkIfRequestedMethod(Method method) {
+
+        String webRoutePath = method.getAnnotation(WebRoute.class).path();
+        METHOD webRouteMethod = method.getAnnotation(WebRoute.class).method();
+
+        boolean pathMatches = requestPath.equals(webRoutePath);
+        boolean methodMatches = requestMethod.equals(webRouteMethod.toString());
+
+        return pathMatches && methodMatches;
     }
 
 }
